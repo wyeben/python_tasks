8c757a3c0fd19b2c551e98b6ba5b719e7ecccc13
Wyeben <yila.ben@yahoo.com>
2023-10-25 10:00:04 +0100

    exercise

--- pythonProject/americana/subsequence.py
+++ pythonProject/americana/subsequence.py
@@ -9,23 +9,12 @@ def subsequence(word1, word2):
     return i == len(word1)
 
 
-def subsequence1(y, t):
-    i = 0
-    r = 0
-    for i in range(len(y)):
-        for r in range(len(t)):
-            if y[i] == t[r]:
-                i += 1
-            r += 1
-    return i == len(y)
-
-
 words1 = 'bce'
 words2 = 'abcde'
 words3 = 'met'
 words4 = 'stream'
 words5 = 'stem'
 words6 = 'stream'
-print(subsequence1(words1, words2))
-print(subsequence1(words3, words4))
-print(subsequence1(words5, words6))
+print(subsequence(words1, words2))
+print(subsequence(words3, words4))
+print(subsequence(words5, words6))
