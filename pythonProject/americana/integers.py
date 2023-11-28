d56d02a0a40b70acc67820970ea384054472b0c2
Wyeben <yila.ben@yahoo.com>
2023-11-28 10:15:49 +0100

    added code

--- pythonProject/americana/integers.py
+++ pythonProject/americana/integers.py
@@ -1,9 +1,18 @@
-def numbers(number):
-    total = []
-    for num in number:
-        total.append(num)
-    return total
+positive = 0
+negative = 0
+zeros = 0
 
+while True:
+    number = int(input('enter a number or (-1) to stop: '))
 
-collect = int(input('enter a number or (s) to stop'))
+    if number == -1:
+        break
 
+    if number == 0:
+        zeros += 1
+    if number > 0:
+        positive += 1
+    if number < 0:
+        negative += 1
+
+print('you have entered positive: ', positive, ' negative: ', negative, ' zeros: ', zeros)
---
+++ pythonProject/americana/reverse_vowels.py
@@ -0,0 +1,22 @@
+def reverse_vowel(ip):
+    vowels = "aeiouAEIOU"
+    ip = list(ip)
+    i, j = 0, len(ip) - 1
+
+    while i < j:
+        if ip[i] in vowels and ip[j] in vowels:
+            ip[i], ip[j] = ip[j], ip[i]
+            i += 1
+            j -= 1
+        elif ip[i] not in vowels:
+            i += 1
+        elif ip[j] not in vowels:
+            j -= 1
+
+    return ''.join(ip)
+
+
+giving1 = "hello"
+giving2 = "leetcode"
+print(reverse_vowel(giving1))
+print(reverse_vowel(giving2))
